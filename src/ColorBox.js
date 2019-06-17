bea52e905863093559d62c0213d1208da1ede666
Hera Kim <herakim@Heras-MBP.fios-router.home>
2019-06-17 15:07:30 -0400

    copying to clipboard and the copy button text changes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 export default class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // value: '',
+            copied: false
+        };
+    }
+
     render() {
         const { name, background } = this.props;
         return (
-            <div className="Color-Box" style={{ background }}>
-              <div className="copy-container">
-                  <div className="box-content">
-                    <span>{name}</span>
-                  </div>
-                  <button className="copy-button">Copy To Clipboard</button>
-              </div>
-              <span className="see-more">More</span>
-            </div>
+            <CopyToClipboard 
+                text={background}
+                onCopy={() => this.setState({ copied: true })}
+            >
+                <div className="Color-Box" style={{ background }}>
+                <div className="copy-container">
+                    <div className="box-content">
+                        <span>{name}</span>
+                    </div>
+                        <button className="copy-button">{this.state.copied ? 'Copied!' : 'Copy To Clipboard'}</button>
+                </div>
+                <span className="see-more">More</span>
+                </div>
+            </CopyToClipboard>
         )
     }
 }
