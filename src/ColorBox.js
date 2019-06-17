fcac93c3f6e12e895bccfa857e0fbb4c480bb396
Hera Kim <herakim@Heras-MBP.fios-router.home>
2019-06-17 15:35:46 -0400

    added overlay animation for color boxes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,24 +6,45 @@ export default class ColorBox extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // value: '',
             copied: false
         };
+        this.handleCopy = this.handleCopy.bind(this);
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+
+    handleCopy() {
+        this.changeCopyState();
+    }
+
+    changeCopyState() {
+        this.setState({ copied: true }, () => {
+            setTimeout(() => {
+                this.setState({ copied: false });
+            }, 1500);
+        });
     }
 
     render() {
         const { name, background } = this.props;
+        const { copied } = this.state;
         return (
             <CopyToClipboard 
                 text={background}
-                onCopy={() => this.setState({ copied: true })}
+                onCopy={this.handleCopy}
             >
                 <div className="Color-Box" style={{ background }}>
+                <div className={`copy-color-overlay ${copied && 'show'}`} style={{ background }}></div>
+
+                <div className={`copy-message ${copied && 'show'}`}>
+                    <h1>Copied!</h1>
+                    <p>{background}</p>
+                </div>
+                
                 <div className="copy-container">
                     <div className="box-content">
                         <span>{name}</span>
                     </div>
-                        <button className="copy-button">{this.state.copied ? 'Copied!' : 'Copy To Clipboard'}</button>
+                        <button className="copy-button">Copy To Clipboard</button>
                 </div>
                 <span className="see-more">More</span>
                 </div>
