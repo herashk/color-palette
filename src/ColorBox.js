7e5aea5b50176cedecbb4843420247404f4fa022
Hera Kim <herakim@Heras-MBP.fios-router.home>
2019-06-17 14:58:06 -0400

    styling added for color box, added copy to clipboard button, see more button

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,10 +3,16 @@ import './ColorBox.css';
 
 export default class ColorBox extends Component {
     render() {
+        const { name, background } = this.props;
         return (
-            <div className="Color-Box" style={{ background: this.props.background }}>
-              <span>{this.props.name}</span>
-              <span>MORE MORE</span>
+            <div className="Color-Box" style={{ background }}>
+              <div className="copy-container">
+                  <div className="box-content">
+                    <span>{name}</span>
+                  </div>
+                  <button className="copy-button">Copy To Clipboard</button>
+              </div>
+              <span className="see-more">More</span>
             </div>
         )
     }
