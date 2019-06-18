4fdca29db0a1c23de6628e0a6bbf9f20815e0c8c
Hera Kim <herakim@Heras-MBP.fios-router.home>
2019-06-17 22:19:17 -0400

    added navbar styling

--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import Slider from 'rc-slider';
 import ColorBox from './ColorBox';
-import 'rc-slider/assets/index.css';
+import Navbar from './Navbar';
 import './Palette.css';
 
 export default class Palette extends Component {
@@ -26,16 +25,7 @@ export default class Palette extends Component {
         ));
         return (
             <div className="Palette">
-                {/* Navbar  */}
-                <div className="slider">
-                    <Slider 
-                        defaultValue={level} 
-                        min={100} 
-                        max={900} 
-                        step={100}
-                        onAfterChange={this.changeLevel}
-                    />
-                </div>
+                <Navbar level={level} changeLevel={this.changeLevel} />
                 <div className="Palette-Colors">
                   {colorBoxes}
                 </div>
