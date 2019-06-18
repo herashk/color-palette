a3a472c6410df62d4f1d534010f3aca38157da26
Hera Kim <herakim@Heras-MBP.fios-router.home>
2019-06-18 10:31:02 -0400

    added palette route /:id

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { Route, Switch } from "react-router-dom";
 import { generateColorPalette } from './helpers/helpers';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  findPalette(id) {
+    return seedColors.find((palette) => palette.id === id );
   }
 
   render() {
     return (
-      <div className="App">
-        <Palette palette={generateColorPalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>palette list goes here</h1>}/>
+        <Route 
+          exact 
+          path="/palette/:id" 
+          render={(routeProps) => (<Palette palette={generateColorPalette(this.findPalette(routeProps.match.params.id))} />)}
+        />
+      </Switch>
+
+
+      // <div className="App">
+      //   <Palette palette={generateColorPalette(seedColors[4])} />
+      // </div>
     );
   }
 }
