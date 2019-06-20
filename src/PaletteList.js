10b555b45dd941b26592516be8899a371037263b
Hera Kim <herakim@Heras-MBP.fios-router.home>
2019-06-19 22:55:53 -0400

    added transition to delete mini palette using react transition library

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/PaletteListStyles';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class PaletteList extends Component {
 
@@ -19,16 +20,19 @@ class PaletteList extends Component {
                         <h1>COLOR PALETTE OPTIONS</h1>
                         <Link to="/palette/new">Create New Palette</Link>
                     </nav>
-                    <div className={classes.palettes}>
-                        {palettes.map(palette => (
-                             <MiniPalette {...palette} 
-                                key={palette.id} 
-                                handleClick={() => this.clickToPalette(palette.id)} 
-                                deletePalette={deletePalette}
-                                id={palette.id}
-                            />
-                         ))}
-                    </div>
+                        <TransitionGroup className={classes.palettes}>
+                            {palettes.map(palette => (
+                                <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+                                    <MiniPalette {...palette} 
+                                        key={palette.id} 
+                                        handleClick={() => this.clickToPalette(palette.id)} 
+                                        deletePalette={deletePalette}
+                                        id={palette.id}
+                                    />
+                                </CSSTransition>
+                            ))}
+
+                        </TransitionGroup>
                 </div>
             </div>
         )
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,30 +1,50 @@
+import sizes from './sizes';
+import bg from './bg.svg';
 
 export default {
+    "@global": { // a way to define a global style
+        ".fade-exit": {
+            opacity: 1
+        },
+        ".fade-exit-active": {
+            opacity: 0,
+            transition: "opacity 500ms ease-out"
+        }
+    },
     root: {
-        backgroundColor: "#101031",
         height: "100vh",
         display: "flex",
         alignItems: "flex-start",
-        justifyContent: "center"
+        justifyContent: "center",
+        /* background by SVGBackgrounds.com */
+        backgroundColor: "#ffffff",
+        backgroundImage: `url(${bg})`,
+        overflow: "scroll",
     },
     container: {
         width: "50%",
         display: "flex",
         alignItems: "flex-start",
         flexDirection: "column",
-        flexWrap: "wrap"
+        flexWrap: "wrap",
+        [sizes.down("xl")]: {
+            width: "80%",
+        },
+        [sizes.down("xs")]: {
+            width: "75%",
+        },
     },
     nav: {
         display: "flex",
         width: "100%",
         justifyContent: "space-between",
-        color: "white",
+        color: "rgb(0, 0, 0, 0.5)",
         alignItems: "center",
         "& h1": {
             fontWeight: "400"
         },
         "& a": {
-            color: "white",
+            color: "rgb(0, 0, 0, 0.5)",
             // textDecoration: "none"
         }
     },
@@ -33,6 +53,13 @@ export default {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "auto auto auto",
-        gridGap: "5%"
+        gridGap: "1.5rem",
+        [sizes.down("xs")]: {
+            gridTemplateColumns: "repeat(1, 100%)"
+        },
+        [sizes.down("md")]: {
+            gridTemplateColumns: "repeat(2, 50%)"
+        },
+
     }
 }
