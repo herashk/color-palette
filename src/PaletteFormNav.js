9ec343a93cb6ae93b92325bfd0764c69ea0fa4ba
Hera Kim <herakim@Heras-MBP.fios-router.home>
2019-06-19 20:52:17 -0400

    finished some minor styling

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -10,45 +10,8 @@ import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
 import { Link } from 'react-router-dom';
 import PaletteMetaForm from './PaletteMetaForm';
-
-const drawerWidth = 400;
-
-const styles = theme => ({
-    root: {
-        display: "flex"
-    },
-    appBar: {
-        transition: theme.transitions.create(["margin", "width"], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen
-        }),
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        height: "64px"
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(["margin", "width"], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-         })
-    },
-    menuButton: {
-        marginLeft: 12,
-        marginRight: 20
-    },
-    navButtons: {
-        marginRight: "1rem",
-        "& a": {
-            textDecoration: "none"
-        }
-    },
-    button: {
-        margin: "0 0.5rem",
-    },
-});
+import styles from './styles/PaletteFormNavStyles';
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 class PaletteFormNav extends Component {
     constructor(props) {
@@ -87,12 +50,12 @@ class PaletteFormNav extends Component {
                 >
                 <Toolbar disableGutters={!open}>
                     <IconButton
-                    color='inherit'
-                    aria-label='Open drawer'
-                    onClick={this.props.handleDrawerOpen}
-                    className={classNames(classes.menuButton, open && classes.hide)}
+                        color='inherit'
+                        aria-label='Open drawer'
+                        onClick={this.props.handleDrawerOpen}
+                        className={classNames(classes.menuButton, open && classes.hide)}
                     >
-                        <MenuIcon />
+                    <ChevronRightIcon />
                     </IconButton>
                     <Typography variant='h6' color='inherit' noWrap>
                         Create a Palette
--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -6,7 +6,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-import { Picker } from "emoji-mart";
+// import { Picker } from "emoji-mart";
 import "emoji-mart/css/emoji-mart.css";
 
 class PaletteMetaForm extends Component {
